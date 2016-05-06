c0aeda4c6189d38ca350e37f6b0c9ed5fbf77806
shigeru <hironoki3@gmail.com>
2016-05-06 20:11:07 +0900

    thread-per-message pattern

---
+++ src/main/java/threadPerMessagePattern/Host.java
@@ -0,0 +1,17 @@
+package threadPerMessagePattern;
+
+/**
+ * Created by ikawa on 2016/05/06.
+ */
+public class Host {
+    private final Helper helper = new Helper();
+    public void request(final int count, final char c) {
+        System.out.println("     request(" + count + ", " + c + ") BEGIN");
+        new Thread() {
+            public void run() {
+                helper.handle(count, c);
+            }
+        }.start();
+        System.out.println("     request(" + count + ", " + c + ") END");
+    }
+}
---
+++ src/main/java/threadPerMessagePattern/Main.java
@@ -0,0 +1,15 @@
+package threadPerMessagePattern;
+
+/**
+ * Created by ikawa on 2016/05/06.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("main BEGIN");
+        Host host = new Host();
+        host.request(10, 'A');
+        host.request(20, 'B');
+        host.request(30, 'C');
+        System.out.println("main END");
+    }
+}
